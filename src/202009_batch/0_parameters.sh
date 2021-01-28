347b6d9e8a3c19aaa45fa01bbf7aee8f10907423
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-01-28 13:25:31 -0800

    add performance eval table

---
+++ src/202009_batch/0_parameters.sh
@@ -0,0 +1,3 @@
+res_d='/oak/stanford/groups/mrivas/projects/PRS/private_output/202009_batch'
+res_scratch_d='/scratch/groups/mrivas/projects/PRS/private_output/202009_batch'
+gdrive_eval_tbl='https://docs.google.com/spreadsheets/d/1n-Lk2ooPJPG7Zbk8Vu43h_n9dzZWvV7elqQyclWvGI8'
