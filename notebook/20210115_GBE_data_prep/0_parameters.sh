9a5be749581fd6f4e833a75501037a41bef8c739
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-02-24 22:33:52 -0800

    update plots

--- notebook/20210115_GBE_data_prep/0_parameters.sh
+++ notebook/20210115_GBE_data_prep/0_parameters.sh
@@ -3,3 +3,5 @@ data_d='../../data'
 res_d='/oak/stanford/groups/mrivas/projects/PRS/private_output/202009_batch'
 res_scratch_d='/scratch/groups/mrivas/projects/PRS/private_output/202009_batch'
 gdrive_eval_tbl='https://docs.google.com/spreadsheets/d/1n-Lk2ooPJPG7Zbk8Vu43h_n9dzZWvV7elqQyclWvGI8'
+
+mrp_blacklist_f='/oak/stanford/groups/mrivas/users/ytanigaw/repos/rivas-lab/ukbb-tools/13_mrp/gbe_blacklist.tsv'
