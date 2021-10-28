7fd98702dcf0eaf1dac59ec8be9c5046d87c2c47
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-10-28 11:15:23 -0700

    add grep-based output check

--- src/20211027_evaluation/snpnet_eval_v2.slurm.sh
+++ src/20211027_evaluation/snpnet_eval_v2.slurm.sh
@@ -7,9 +7,7 @@ SRCDIR=$(dirname ${SRCNAME})
 slurm_log_d=${SRCDIR}/slurm_logs
 slurm_job_name=$(basename ${SRCNAME%.slurm.sh})
 job_list_sh=${slurm_log_d}/${slurm_job_name}.jobs.sh
-batch_size=2 # the number of jobs executed in an array task in SLURM.
-memory=12000
-cpus=1
+batch_size=1 # the number of jobs executed in an array task in SLURM.
 sbatch_resources_str='-p mrivas --qos=high_p --nodes=1 --mem=12000 --cores=1 --time=2-00:00'
 
 #############################
@@ -20,15 +18,14 @@ source paths.sh
 
 cat_or_zcat () {
     local file=$1
-    if [ "${file%.gz}.gz" == "${file}" ] || [ "${file%.bgz}.bgz" == "${file}" ] ; then 
-        zcat ${file} 
-    elif [ "${file%.zst}.zst" == "${file}" ] ; then 
-        zstdcat ${file}
+    if [ "${file%.gz}.gz" == "${file}" ] || [ "${file%.bgz}.bgz" == "${file}" ] ; then
+        zcat "${file}"
+    elif [ "${file%.zst}.zst" == "${file}" ] ; then
+        zstdcat "${file}"
     else
-        cat ${file}
+        cat "${file}"
     fi
 }
-
 
 if [ ! -d "${slurm_log_d}" ] ; then mkdir -p "${slurm_log_d}" ; fi
 
@@ -36,24 +33,23 @@ out_dir="${project_d}/private_output/$(basename ${SRCDIR})/output"
 
 if [ ! -d "${out_dir}" ] ; then mkdir -p "${out_dir}" ; fi
 
+find "${out_dir}"  -type f | sort > "${slurm_log_d}/output_files.txt"
+
 # cat_or_zcat ${project_d}/${PRS_f} | awk 'NR==1' | tr '\t' '\n' | awk 'NR>94' \
-! cat_or_zcat ${project_d}/${PRS_f} | head -n1 | tr '\t' '\n' \
+! cat_or_zcat "${project_d}/${PRS_f}" | head -n1 | tr '\t' '\n' \
 | awk 'NR>94' | sed -e 's/^PRS_//' \
 | while read -r GBE_ID ; do
 
     out_prefix="${out_dir}/${GBE_ID}"
 
-#     if  [ ! -s "${out_prefix}.log"            ] ||
-#         [ ! -s "${out_prefix}.covarBETAs.tsv" ] ||
-#         [ ! -s "${out_prefix}.eval.tsv"       ] ||
-#         [ ! -s "${out_prefix}.percentile.tsv" ] ||
-#         [ ! -s "${out_prefix}.plot.pdf"       ] ||
-#         [ ! -s "${out_prefix}.plot.png"       ] ||
-#         [ ! -s "${out_prefix}.PRS_pval.tsv"   ] ; then
-    
-        echo "bash ${SRCNAME%.slurm.sh}.sh ${GBE_ID}"
+    # we will check the resulting files
+    grep_pattern="${out_prefix}.log|${out_prefix}.covarBETAs.tsv|${out_prefix}.eval.tsv|${out_prefix}.percentile.tsv|${out_prefix}.plot.pdf|${out_prefix}.plot.png|${out_prefix}.PRS_pval.tsv"
 
-#     fi
+    grep_wc_l=$(grep -E "${grep_pattern}" "${slurm_log_d}/output_files.txt" | wc -l)
+
+    if [ "${grep_wc_l}" -ne 7 ] ; then
+        echo "bash ${SRCNAME%.slurm.sh}.sh ${GBE_ID}"
+    fi
 done > "${job_list_sh}"
 
 #############################
@@ -67,6 +63,7 @@ n_array_tasks=$(   perl -e "print(int(  (${batch_size} - 1 + ${n_jobs}) / ${batc
 #############################
 parasol_sbatch_sh="${HOME}/repos/yk-tanigawa/resbatch/parasol-sbatch.sh"
 echo "## submission of ${n_jobs} jobs in ${n_array_tasks} tasks (each has up to ${batch_size} jobs) ##"
+echo "## ${job_list_sh} ##"
 
 (
     # use sub-shell and print the executed command with set -x
